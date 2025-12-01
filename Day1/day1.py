12a363aa88433ecc2e13240b3a7ed6c2dfb3223c
nohaxjustpug <88031123+nohaxjustpug@users.noreply.github.com>
2025-12-01 13:57:37 -0500

    Finished day 1 code

--- Day1/day1.py
+++ Day1/day1.py
@@ -1,13 +1,29 @@
 class Dial:
     def __init__(self):
         self.number = 50
+        self.zeros = 0
 
-    def rotate(self, direction):
-        if direction == "L":
-            self.number -= 1
-            if self.number < 0:
-                self.number = 99
-        else:
-            self.number += 1
-            if self.number > 99:
-                self.number = 0
+    def rotate(self, direction, rotations):
+        for i in range(int(rotations)):
+            if direction == "L":
+                self.number -= 1
+                if self.number < 0:
+                    self.number = 99
+            else:
+                self.number += 1
+                if self.number > 99:
+                    self.number = 0
+        
+            if self.number == 0:
+                self.zeros += 1
+
+if __name__ == "__main__":
+    dial = Dial()
+
+    with open("input.txt", "r") as f:
+        inputs = f.readlines()
+        for input in inputs:
+            dir = input[0]
+            rots = input[1:]
+            print(f"#0: {dial.zeros:05d} | {dial.number}\t->\t{dir}:{rots}")
+            dial.rotate(dir, str(rots))
